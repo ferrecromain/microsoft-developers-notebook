61bb2b77f079c06042054c055815b611648df6a5
Ferrec Romain <5568618+ferrecromain@users.noreply.github.com>
2022-06-04 22:55:36 +0200

    [Core] Added User entity

---
+++ DotnetSampleSolution.Core/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetSampleSolution.Core.Entities
+{
+    internal class UserEntity
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string? Email { get; set; }
+    }
+}
